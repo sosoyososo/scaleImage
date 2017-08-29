9e9a8da17e16909183ddd7b6895966ead3efa209
Karsa Wang <wanghongtao114@gamil.com>
2017-08-29 19:33:08 +0800

    first commit

---
+++ ImageScale/ImageScaleUtil.swift
@@ -0,0 +1,9 @@
+//
+//  ImageScaleUtil.swift
+//  ImageScale
+//
+//  Created by karsa on 2017/8/29.
+//  Copyright © 2017年 karsa. All rights reserved.
+//
+
+import Foundation
---
+++ ImageScale/SizeListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SizeListViewController.swift
+//  ImageScale
+//
+//  Created by karsa on 2017/8/29.
+//  Copyright © 2017年 karsa. All rights reserved.
+//
+
+import Cocoa
+
+class SizeListViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
